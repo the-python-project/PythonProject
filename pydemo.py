c607b709c4e892e7a73729af982267cb33ee6391
MayKikuchi <m.may.kikuchi@gmail.com>
2019-08-18 14:50:06 -0700

    added some class functionality, and started the Clues class, added a blank Deck class, also included a __init__.py file and a deckWords.txt file

--- pydemo.py
+++ pydemo.py
@@ -1,21 +1,29 @@
-print("hello world")
-a = 1+1
-print ("the value of a is " + str(a))
-print (1+1)
-for x in range(1, 10):
-    print(x)
-    if x%2 == 0:
-        print("x minus 1 is: " +str(x-1))
-print("loop is done")
-#this is a comment
-""" this is a docstring comment"""
-b="hi there :)"
-aAndBBool = a is b
-print("is a and b the same type? " + str(aAndBBool))
-print(b[4]) #should be h
-input = input()
-print("this is the input: " +input)
-print("HI May - from Buffy")
-print("HI there")
-print("What is up!!!!")
+import Clues
+# print("hello world")
+# a = 1+1
+# print ("the value of a is " + str(a))
+# print (1+1)
+# for x in range(1, 10):
+#     print(x)
+#     if x%2 == 0:
+#         print("x minus 1 is: " +str(x-1))
+# print("loop is done")
+# #this is a comment
+# """ this is a docstring comment"""
+# b="hi there :)"
+# aAndBBool = a is b
+# print("is a and b the same type? " + str(aAndBBool))
+# print(b[4]) #should be h
+# input = input()
+# print("this is the input: " +input)
+# print("HI May - from Buffy")
+# print("HI there")
+# print("What is up!!!!")
 
+def main():
+    clues = Clues.Clues()
+    clue1 = clues.backboneClues[0]
+    print(clue1)
+
+if __name__ == "__main__":
+    main()
